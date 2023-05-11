31b834f54e95e1a1bc76132b9ba7810ab6ca9dce
arthur <justarthur.t@gmail.com>
2023-05-11 14:25:09 +0800

    Refactor code with best practises and added readme.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SeleniumProject
+
+To automate the below scenario:
+
+1) Go to tokopedia.com, search "iphone 14 pro".
+2) Filter the price with min. 100.000, max 30.000.000, filter only from the “Official Store”, sort from the “Harga Terendah”.
+3) Obtain all the item names from page 1 until 3 and print on the terminal. 
+
+## How to run?
+* Select your preferred development environment. This project is developed using:
+  * [IntelliJ IDEA](https://www.jetbrains.com/idea/download/#section=mac) as the Java IDE. 
+  * Maven as a build tool. 
+  * Openjdk version "20.0.1".
+* Install [Selenium library](https://www.selenium.dev/documentation/webdriver/getting_started/install_library/).
+* Download [ChromeDriver](https://chromedriver.chromium.org/downloads).
+* After cloning this repository, update the code with your own chrome driver directory. 
+* Press Ctrl+R or click the green arrow button in the gutter to run the code.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        System.out.printf("Hello and welcome!");
-
-        // Starting Driver
+        // Starting Driver (REPLACE BELOW with your own chrome driver directory)
         System.setProperty("webdriver.chrome.driver","/Users/att/Downloads/chromedriver_mac64/chromedriver");
 
         // Chrome browser configurations
@@ -26,35 +24,35 @@ public class Main {
         WebDriver driver= new ChromeDriver(options);
         driver.manage().window().maximize();
 
+        // Wait up to maximum 30 seconds for each object. (To be decided based on your specs)
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
         // Go to tokopedia website
         driver.get("https://www.tokopedia.com/");
 
         // Search for iphone 14 pro
-        //driver.findElement(By.xpath("//input[@data-unify='Search']")).sendKeys("iphone 14 pro"+ Keys.RETURN);
         driver.findElement(By.cssSelector("input[data-unify=Search]")).sendKeys("iphone 14 pro" + Keys.RETURN);
-
-        // Wait up to maximum 30 seconds for each objects
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-        driver.findElement(By.cssSelector("input[value='Official Store']+span")).click();
 
         // Filter price with min. 100.000, max 30.000.000
         driver.findElement(By.cssSelector("input[data-testid=iptSRPMinPriceFilter]")).sendKeys("100000" + Keys.RETURN);
         driver.findElement(By.cssSelector("input[data-testid=iptSRPMaxPriceFilter]")).sendKeys("30000000" + Keys.RETURN);
 
+        // Filter only from the "Official store"
+        driver.findElement(By.cssSelector("input[value='Official Store']+span")).click();
+
         // Sort result from the lowest price
         driver.findElement(By.cssSelector("button[data-unify=Select]")).click();
         driver.findElement(By.cssSelector("button[data-item-text='Harga Terendah']")).click();
 
+        // Logic to print all the item names from page 1 until 3
         List <String> allItemNames = new ArrayList<>();
         int num=0;
 
-        // Pagination Loop
+        // Pagination Loop for 1-3
         for (int i=1; i<=3; i++){
 
             // Scroll to page most bottom
             ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
-            Thread.sleep(3000);
 
             if (i>1) {
                 // Click on next page
@@ -62,23 +60,16 @@ public class Main {
             }
 
                 // Loop to get all item names
-                List<WebElement> SearchedItemNames = driver.findElements(By.cssSelector("[data-testid='spnSRPProdName']"));
+                List<WebElement> searchedItemNames = driver.findElements(By.cssSelector("[data-testid='spnSRPProdName']"));
 
-                for (WebElement itemName : SearchedItemNames) {
+                for (WebElement itemName : searchedItemNames) {
                     num++;
                     System.out.println("\n" + num +". Item name: " + itemName.getText());
                     allItemNames.add(itemName.getText());
                 }
-                System.out.println("\n Page "+i+" elements count: " + SearchedItemNames.size());
+                System.out.println("\n Page "+i+" elements count: " + searchedItemNames.size());
         }
 
-//        driver.findElement(By.cssSelector("button[aria-label='Laman 2']")).click();
-//
-//        driver.findElement(By.cssSelector("button[aria-label='Laman 3']")).click();
-
-
         driver.quit();
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
     }
 }
